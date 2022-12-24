3703e949ed216ef5f8feeb9b509ffb2ebad000ad
yuhiko1122 <100394481+yuhiko1122@users.noreply.github.com>
2022-12-24 10:17:34 +0900

    part4
    そこそこ

---
+++ my-new-project/screens/NewsScreen.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { FlatList, StyleSheet, SafeAreaView, View } from "react-native";
+import NewsKizi from "../components/NewsKizi";
+import Constants from "expo-constants";
+import axios from "axios";
+
+const URI = `https://newsapi.org/v2/top-headlines?country=jp&category=entertainment&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
+export default function NewsScreen() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  const getNews = async () => {
+    const response = await axios.get(URI);
+    setNews(response.data.articles);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={news}
+        renderItem={({ item }) => (
+          <NewsKizi
+            imageuri={item.urlToImage}
+            title={item.title}
+            subtext={item.publishedAt}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
